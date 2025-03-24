4248f612cd745d2a0ad32a67050857cb8cdfad0a
dvviana <davivviana@gmail.com>
2025-03-24 10:37:31 -0300

    add About and Contact components; update app routing and styles; enhance layout and CSS for improved aesthetics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CarsComponent } from './cars/cars.component';
 import { RentComponent } from './rent/rent.component';
-
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'cars', component: CarsComponent},
   {path: 'rent', component: RentComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { CarsComponent } from './cars/cars.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RentComponent } from './rent/rent.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
     LoginComponent,
-    RentComponent
+    RentComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
